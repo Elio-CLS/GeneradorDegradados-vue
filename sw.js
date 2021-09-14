f60fad56ba655dfb8e4486f7b22d6574c17f8694
Elio-CLS <80856380+Elio-CLS@users.noreply.github.com>
2021-09-14 04:40:45 -0300

    PWA - Manifest - Icons - Fallback

---
+++ sw.js
@@ -0,0 +1,67 @@
+const CACHE_NAME = "v1_cache_degradados_css";
+const urlsToCache = [
+  "./",
+  "./?umt_source=web_app_manifest",
+  "./pages/fallback.html",
+  "./css/styles.css",
+  "./img/favicon.png",
+  "./img/icon32.png",
+  "./img/icon64.png",
+  "./img/icon128.png",
+  "./img/icon192.png",
+  "./img/icon256.png",
+  "./img/icon512.png",
+  "./img/icon1024.png",
+  "./js/main.js",
+  "https://unpkg.com/vue@next",
+  "./js/mountApp.js",
+  "./css/style.css",
+  "./css/normalize.css",
+  "./manifest.json",
+  "https://fonts.googleapis.com/css2?family=Open+Sans&family=PT+Sans:wght@400;700&display=swap"
+];
+
+self.addEventListener("install", (e) => {
+    e.waitUntil(
+      caches.open(CACHE_NAME).then((cache) =>
+        cache
+          .addAll(urlsToCache)
+          .then(() => self.skipWaiting())
+          .catch((err) => console.log(err))
+      )
+    );
+  });
+  
+  self.addEventListener("activate", (e) => {
+    const cacheWhitelist = [CACHE_NAME];
+  
+    e.waitUntil(
+      caches
+        .keys()
+        .then((cacheNames) => {
+          return Promise.all(
+            cacheNames.map((cacheName) => {
+              if (cacheWhitelist.indexOf(cacheName) === -1) {
+                return caches.delete(cacheName);
+              }
+            })
+          );
+        })
+        .then(() => self.clients.claim())
+    );
+  });
+  
+  self.addEventListener("fetch", (e) => {
+    e.respondWith(
+      caches
+        .match(e.request)
+        .then((res) => {
+          if (res) {
+            return res;
+          }
+  
+          return fetch(e.request);
+        })
+        .catch(() => caches.match("./pages/fallback.html"))
+    );
+  });
